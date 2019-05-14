f428e7fd959e43ff089f083822133c1720e54980
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-14 16:55:03 +0200

    Add colors file

---
+++ MovieApp/Helpers/PTColors.swift
@@ -0,0 +1,41 @@
+//
+//  PTColors.swift
+//  ios-patient-app
+//
+//  Created by Esraa Mohamed Ragab on 10/3/18.
+//  Copyright © 2018 Digital Wellnez. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    var black: UIColor { return UIColor(red: 49/255, green: 49/255, blue: 49/255, alpha: 1) }
+    var underlineGray: UIColor { return UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 1) }
+    var cherryRed: UIColor { return UIColor(red: 237/255, green: 28/255, blue: 36/255, alpha: 1) }
+    var darkGrey: UIColor { return UIColor(red: 154/255, green: 153/255, blue: 153/255, alpha: 1) }
+    var greenyBlue: UIColor { return UIColor(red: 64/255, green: 186/255, blue: 136/255, alpha: 1) }
+    var greenyBlueTwo: UIColor { return UIColor(red: 85/255, green: 198/255, blue: 151/255, alpha: 1) }
+    var lightGray: UIColor { return UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1) }
+    var purple: UIColor { return UIColor(red: 135/255, green: 129/255, blue: 189/255, alpha: 1) }
+    var textorange: UIColor { return UIColor(red: 255/255, green: 150/255, blue: 0, alpha: 1) }
+    var underLineGrey: UIColor { return UIColor(red: 169/255, green: 169/255, blue: 169/255, alpha: 1) }
+    var veryLightPink: UIColor { return UIColor(red: 234/255, green: 234/255, blue: 234/255, alpha: 1) }
+    var veryLightPinkTwo: UIColor { return UIColor(red: 194/255, green: 194/255, blue: 194/255, alpha: 1) }
+    var white: UIColor { return UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1) }
+    var windowsBlueTwo: UIColor { return UIColor(red: 58/255, green: 119/255, blue: 190/255, alpha: 1) }
+    var transparrentGray: UIColor { return UIColor(red: 0, green: 0, blue: 0, alpha: 0.3) }
+    var clearColor: UIColor { return UIColor(red: 0, green: 0, blue: 0, alpha: 0) }
+    var lightBlue: UIColor { return UIColor(red: 151/255, green: 211/255, blue: 243/255, alpha: 1) }
+    var veryLightGray: UIColor { return UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1) }
+    var offWhiteTwo: UIColor { return UIColor(red: 255/255, green: 247/255, blue: 229/255, alpha: 1) }
+    var veryLightGreen: UIColor { return UIColor(red: 209/255, green: 255/255, blue: 213/255, alpha: 1) }
+    var paleGrey: UIColor { return UIColor(red: 236/255, green: 237/255, blue: 241/255, alpha: 1) }
+    var pigPink: UIColor { return UIColor(red: 237/255, green: 133/255, blue: 135/255, alpha: 1) }
+    var lipstick: UIColor { return UIColor(red: 217/255, green: 36/255, blue: 40/255, alpha: 1) }
+    var cornflower: UIColor { return UIColor(red: 99/255, green: 102/255, blue: 238/255, alpha: 1) }
+    var orangeYellow: UIColor { return UIColor(red: 254/255, green: 161/255, blue: 0/255, alpha: 1) }
+    var fadedRed: UIColor { return UIColor(red: 225/255, green: 75/255, blue: 96/255, alpha: 1) }
+    var perrywinkle: UIColor { return UIColor(red: 137/255, green: 145/255, blue: 240/255, alpha: 1) }
+    var rosePink: UIColor { return UIColor(red: 249/255, green: 136/255, blue: 159/255, alpha: 1) }
+//    var orangeYellow: UIColor { return UIColor(red: 249/255, green: 0/255, blue: 0/255, alpha: 1) }
+}
