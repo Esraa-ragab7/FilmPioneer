fab5c092c59c5c2205f13ea435f4295fa57442e6
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-15 14:53:19 +0200

    add button extension have loading indicator method.

---
+++ MovieApp/Helpers/ButtonExtention.swift
@@ -0,0 +1,33 @@
+//
+//  ButtonExtention.swift
+//  MovieApp
+//
+//  Created by Esraa Mohamed Ragab on 5/15/19.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func loadingIndicator(_ show: Bool) {
+        let tag = 808404
+        if show {
+            self.isEnabled = false
+            self.alpha = 0.5
+            let indicator = UIActivityIndicatorView()
+            let buttonHeight = self.bounds.size.height
+            let buttonWidth = self.bounds.size.width
+            indicator.center = CGPoint(x: buttonWidth-40, y: buttonHeight/2)
+            indicator.tag = tag
+            self.addSubview(indicator)
+            indicator.startAnimating()
+        } else {
+            self.isEnabled = true
+            self.alpha = 1.0
+            if let indicator = self.viewWithTag(tag) as? UIActivityIndicatorView {
+                indicator.stopAnimating()
+                indicator.removeFromSuperview()
+            }
+        }
+    }
+}
