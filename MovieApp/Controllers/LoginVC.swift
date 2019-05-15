8eda2f2633f68f44e137236760d3febd9da3ee07
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-15 14:54:47 +0200

    Complete login page with network.

--- MovieApp/Controllers/LoginVC.swift
+++ MovieApp/Controllers/LoginVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginVC.swift
 //  MovieApp
 //
 //  Created by Esraa Mohamed Ragab on 5/13/19.
@@ -8,10 +8,11 @@
 import UIKit
 import Foundation
 
-class ViewController: BaseVC {
+class LoginVC: BaseVC {
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var showPassword: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +29,70 @@ class ViewController: BaseVC {
         password.isSecureTextEntry ? showPassword.setImage(#imageLiteral(resourceName: "closeEye"), for: UIControl.State.normal) : showPassword.setImage(#imageLiteral(resourceName: "openEye"), for: UIControl.State.normal)
     }
     
+    func enableButtonAndTextFields(){
+        loginButton.loadingIndicator(false)
+        password.allowsEditingTextAttributes = true
+        userName.allowsEditingTextAttributes = true
+        showPassword.isUserInteractionEnabled = true
+    }
+    
+    func disableButtonAndTextFields(){
+        loginButton.loadingIndicator(true)
+        password.allowsEditingTextAttributes = false
+        userName.allowsEditingTextAttributes = false
+        showPassword.isUserInteractionEnabled = false
+    }
+    
+    func getSessionId(request_token: String){
+        let parameters = [
+            "request_token": request_token
+        ]
+        var headers = [requestHeaders]()
+        headers.append(requestHeaders(key:"Content-Type",value:"application/json"))
+        NetworkManager.sharedInstance.serverRequests(url: "https://api.themoviedb.org/3/authentication/session/new?api_key=506b23ad4ef222b830bab9a275fa942c", method: .post, parameters: parameters, headers: headers, success: { (res) in
+            self.enableButtonAndTextFields()
+            AppDelegate.session_id = (res["session_id"] as! String)
+        }) { (error) in
+            self.enableButtonAndTextFields()
+            self.Alert(title: "Error!", message: error["status_message"] as? String ?? "Error", VC: self)
+        }
+    }
+    
+    func loginAction(request_token: String){
+        let parameters = [
+            "username": userName.text!,
+            "password": password.text!,
+            "request_token": request_token
+            ]
+        var headers = [requestHeaders]()
+        headers.append(requestHeaders(key:"Content-Type",value:"application/json"))
+        NetworkManager.sharedInstance.serverRequests(url: "https://api.themoviedb.org/3/authentication/token/validate_with_login?api_key=506b23ad4ef222b830bab9a275fa942c", method: .post, parameters: parameters, headers: headers, success: { (res) in
+            self.getSessionId(request_token: res["request_token"] as! String)
+        }) { (error) in
+            self.enableButtonAndTextFields()
+            self.Alert(title: "Error!", message: error["status_message"] as? String ?? "Error", VC: self)
+        }
+    }
+    
     @IBAction func login(_ sender: Any) {
-        
+        disableButtonAndTextFields()
+        if userName.text == "" {
+            enableButtonAndTextFields()
+            Alert(title: "Complete", message: "Please Insert Your User Name.", VC: self)
+        } else if password.text == "" {
+            enableButtonAndTextFields()
+            Alert(title: "Complete", message: "Please Insert Your Password.", VC: self)
+        } else if NetworkManager.sharedInstance.isConnected() {
+            NetworkManager.sharedInstance.serverRequests(url: "https://api.themoviedb.org/3/authentication/token/new?api_key=506b23ad4ef222b830bab9a275fa942c", method: .get, success: { (res) in
+                self.loginAction(request_token: res["request_token"] as! String)
+            }) { (error) in
+                self.enableButtonAndTextFields()
+                self.Alert(title: "Error!", message: "sdsd", VC: self)
+            }
+        } else {
+            enableButtonAndTextFields()
+            Alert(title: "Error!", message: "Please Connect to the Internet..", VC: self)
+        }
     }
     
     
--- MovieApp/Helpers/NetworkManager.swift
+++ MovieApp/Helpers/NetworkManager.swift
@@ -5,4 +5,102 @@
 //  Created by Esraa Mohamed Ragab on 5/14/19.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+class NetworkManager: NSObject {
+    
+    // MARK: - Properties
+    
+    let reachabilityManager:NetworkReachabilityManager?
+    
+    static let sharedInstance = NetworkManager()
+    
+    // MARK: - Initialization
+    
+    private override init() {
+        
+        reachabilityManager = NetworkReachabilityManager(host: "www.google.com")
+    }
+    
+    // MARK: - Accessors
+    
+    func isConnected() -> Bool {
+        return reachabilityManager?.isReachable ?? false
+    }
+    
+    func isConnected3G() -> Bool {
+        return reachabilityManager?.isReachableOnWWAN ?? false
+    }
+    
+    func serverRequests(url :String,
+                        method :HTTPMethod = .get,
+                        parameters:[String:Any] = [:],
+                        headers : [requestHeaders] = [],
+                        arrayParameter: [[String:Any]] = [],
+                        success:@escaping ([String:Any]) -> Void,
+                        failure:@escaping ([String:Any]) -> Void ) {
+        
+        let RequestURL = URL(string: url)
+        var request = URLRequest(url: RequestURL!)
+        request.httpMethod = method.rawValue
+        
+        for header in headers {
+            request.addValue(header.value, forHTTPHeaderField: header.key)
+        }
+        
+        if AppDelegate.session_id != nil{
+            request.addValue(AppDelegate.session_id!, forHTTPHeaderField: "Authorization")
+        }
+        
+        if parameters.count > 0{
+            let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+            request.httpBody = httpBody
+        } else if arrayParameter.count > 0 {
+            let httpBody = try? JSONSerialization.data(withJSONObject: arrayParameter, options: [])
+            request.httpBody = httpBody
+        }
+        
+        Alamofire.request(request).responseData{ response in
+            print(response.response?.statusCode)
+            if response.response?.statusCode == 200 || response.response?.statusCode == 201{
+                do {
+                    let readableJson = try JSONSerialization.jsonObject(with: response.data!, options: .allowFragments)
+                    if let respoDict = readableJson as? [String:Any] {
+                        print(respoDict)
+                        success(respoDict)
+                    }
+                    else{
+                        success(["msg":"success"])
+                    }
+                }catch {
+                    do {
+                        let readableJson = try JSONSerialization.jsonObject(with: response.data!, options: .allowFragments)
+                        if let respoDict = readableJson as? [String:Any] {
+                            print(respoDict)
+                            failure(respoDict)
+                        }
+                    }catch {
+                        success(["successMsg":"done"])
+                    }
+                }
+            }else{
+                do {
+                    let readableJson = try JSONSerialization.jsonObject(with: response.data!, options: .allowFragments)
+                    if let respoDict = readableJson as? [String:Any] {
+                        print(respoDict)
+                        failure(respoDict)
+                    }
+                }catch {
+                    failure(["msg":"Time Out"])
+                }
+            }
+        }
+    }
+    
+}
+
+struct requestHeaders{
+    var key = String()
+    var value = String()
+}
