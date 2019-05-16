af44fc84f4f8afbf77f1dde680c9e97f9083ee0c
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-16 02:16:56 +0200

    Add subRaw Cell to display grid view

---
+++ MovieApp/Controllers/Cells/SubRawCell.swift
@@ -0,0 +1,17 @@
+//
+//  SubRawCell.swift
+//  MovieApp
+//
+//  Created by Esraa Mohamed Ragab on 5/16/19.
+//
+
+import UIKit
+
+class SubRawCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
