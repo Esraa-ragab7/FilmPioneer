84dfccbc6f84455de1b89bcbdb57215d6f30e4cb
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-16 04:45:59 +0200

    Add Account View Controller

---
+++ MovieApp/Controllers/AccountVC.swift
@@ -0,0 +1,29 @@
+//
+//  AccountVC.swift
+//  MovieApp
+//
+//  Created by Esraa Mohamed Ragab on 5/16/19.
+//
+
+import UIKit
+
+class AccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
