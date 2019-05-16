77cc072c008e391ffd8a2fec93c14e1230fbfc63
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-16 04:05:18 +0200

    Add movie Details view Controller

---
+++ MovieApp/Controllers/MovieDetails.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetails.swift
+//  MovieApp
+//
+//  Created by Esraa Mohamed Ragab on 5/16/19.
+//
+
+import UIKit
+
+class MovieDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
