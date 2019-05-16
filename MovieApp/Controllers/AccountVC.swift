bee680e30ab742a8053c776bd4f0442aa2717897
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-16 06:06:17 +0200

    Complete account viewController UI and logic

--- MovieApp/Controllers/AccountVC.swift
+++ MovieApp/Controllers/AccountVC.swift
@@ -7,23 +7,98 @@
 
 import UIKit
 
-class AccountVC: UIViewController {
-
+class AccountVC: BaseVC {
+    // MARK: - outlet
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    @IBOutlet weak var reloadButton: UIButton!
+    
+    // MARK: - variables
+    var profile: Account = Account.init(fromDictionary: [:])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        getProfileData()
     }
-    */
+    
+    // MARK: - GetProgileMethods
+    func getProfileData(){
+        if NetworkManager.sharedInstance.isConnected() {
+            loading.startAnimating()
+            contentView.isHidden = true
+            let session_id = UserDefaults.standard.string(forKey: "sessionID")
+            NetworkManager.sharedInstance.serverRequests(url: "https://api.themoviedb.org/3/account?api_key=\(Constants.api.api_key.rawValue)&session_id=\(session_id ?? "")", success: { (res) in
+                self.profile = Account.init(fromDictionary: res)
+                UserDefaults.standard.set(res, forKey: "profile")
+                self.setUpData()
+                self.successCall()
+            }) { (error) in
+                if let profileData = UserDefaults.standard.dictionary(forKey: "profile") {
+                    self.profile = Account.init(fromDictionary: profileData)
+                    self.setUpData()
+                } else {
+                    self.faildCall()
+                    self.Alert(title: "Error!", message: error["status_message"] as? String ?? "Error", VC: self)
+                }
+            }
+        } else {
+            if let profileData = UserDefaults.standard.dictionary(forKey: "profile") {
+                profile = Account.init(fromDictionary: profileData)
+                setUpData()
+            } else {
+                faildCall()
+                Alert(title: "Error!", message: "Please Connect to the Internet..", VC: self)
+            }
+        }
+    }
+    
+    func setUpData(){
+        self.avatarImage.kf.indicatorType = .activity
+        self.avatarImage.kf.setImage(with: URL(string: self.profile.gravatar), placeholder: #imageLiteral(resourceName: "placeholder"))
+        self.name.text = self.profile.name
+        self.userName.text = self.profile.username
+    }
+    
+    func faildCall(){
+        self.loading.stopAnimating()
+        self.contentView.isHidden = true
+        self.reloadButton.isHidden = false
+    }
+    
+    func successCall(){
+        self.loading.stopAnimating()
+        self.contentView.isHidden = false
+        self.reloadButton.isHidden = true
+    }
+    
+    // MARK: - Button Actions
+    @IBAction func reloadProfile(_ sender: Any) {
+        getProfileData()
+    }
+    
+    @IBAction func logOut(_ sender: Any) {
+        if NetworkManager.sharedInstance.isConnected() {
+            let parameters = [
+                "session_id": UserDefaults.standard.string(forKey: "sessionID")
+            ]
+            var headers = [requestHeaders]()
+            headers.append(requestHeaders(key:"Content-Type",value:"application/json"))
+            
+            NetworkManager.sharedInstance.serverRequests(url: "https://api.themoviedb.org/3/authentication/session?api_key=\(Constants.api.api_key.rawValue)", method: .delete, parameters: parameters as [String : Any], headers: headers, success: { (res) in
+                AppDelegate.shared.rootViewController.switchToLogout()
+            }) { (error) in
+                self.Alert(title: "Error!", message: error["status_message"] as? String ?? "Error", VC: self)
+            }
+        } else {
+            Alert(title: "Error!", message: "Please Connect to the Internet..", VC: self)
+        }
+    }
+    
 
 }
