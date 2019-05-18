9ba10d08a552f849ae5090518e6df7e50e7e25f6
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-18 05:00:51 +0200

    add get Movies method to movies manager.

--- MovieApp/Network/Authentication/MoviesManager.swift
+++ MovieApp/Network/Authentication/MoviesManager.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import Alamofire
+
+class MoviesManager {
+    
+    static func getTopRatedMovies(pageNum: String, completionHandler: @escaping ([String:Any]?, [String:Any]?) -> Void) {
+        NetworkManager.sharedInstance.serverRequests(url: "https://api.themoviedb.org/3/movie/top_rated?api_key=\(Constants.api.api_key.rawValue)&language=en-US&page=\(pageNum)", success: { (res) in
+            completionHandler(res, nil)
+        }) { (error) in
+            completionHandler(nil, error)
+        }
+    }
+    
+}
+
