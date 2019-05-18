688fcd65f7654dd6ee42f05a5290c7984ec7b4d4
esraa ragab <esraa.mo.ragab@gmail.com>
2019-05-18 15:39:22 +0200

    Clean account view controller code.

--- MovieApp/View Controllers/AccountViewController.swift
+++ MovieApp/View Controllers/AccountViewController.swift
@@ -27,6 +27,8 @@ class AccountViewController: BaseViewController {
     private let fromAnimation = AnimationType.from(direction: .right, offset: 250.0)
     private var profile: Account = Account.init(fromDictionary: [:])
     
+    // MARK: - ViewController Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -35,30 +37,10 @@ class AccountViewController: BaseViewController {
         getProfileData()
     }
     
-    // MARK: - GetProgileMethods
+    // MARK: - GetProgileMethod
+    
     func getProfileData(){
-        if NetworkManager.sharedInstance.isConnected() {
-            loading.startAnimating()
-            contentView.isHidden = true
-            let session_id = UserDefaults.standard.string(forKey: "sessionID")
-            NetworkManager.sharedInstance.serverRequests(url: "https://api.themoviedb.org/3/account?api_key=\(Constants.api.api_key.rawValue)&session_id=\(session_id ?? "")", success: { (res) in
-                self.profile = Account.init(fromDictionary: res)
-                UserDefaults.standard.set(res, forKey: "profile")
-                self.setUpData()
-                self.successCall()
-            }) { (error) in
-                if error["status_code"] as? Int == 3 {
-                    self.logOut()
-                } else if let profileData = UserDefaults.standard.dictionary(forKey: "profile") {
-                    self.profile = Account.init(fromDictionary: profileData)
-                    self.setUpData()
-                    self.successCall()
-                } else {
-                    self.faildCall()
-                    self.Alert(title: "Error!", message: error["status_message"] as? String ?? "Error", VC: self)
-                }
-            }
-        } else {
+        guard NetworkManager.sharedInstance.isConnected() else {
             if let profileData = UserDefaults.standard.dictionary(forKey: "profile") {
                 profile = Account.init(fromDictionary: profileData)
                 setUpData()
@@ -66,10 +48,68 @@ class AccountViewController: BaseViewController {
                 faildCall()
                 Alert(title: "Error!", message: "Please Connect to the Internet..", VC: self)
             }
+            return
         }
+        loading.startAnimating()
+        contentView.isHidden = true
+        AccountManager.getProfile(completionHandler: { (res, error) in
+            if error != nil {
+                if error!["status_code"] as? Int == 3 {
+                    self.logOut()
+                } else if let profileData = UserDefaults.standard.dictionary(forKey: "profile") {
+                    self.profile = Account.init(fromDictionary: profileData)
+                    self.setUpData()
+                    self.successCall()
+                } else {
+                    self.faildCall()
+                    self.Alert(title: "Error!", message: error!["status_message"] as? String ?? "Error", VC: self)
+                }
+                return
+            }
+            self.profile = Account.init(fromDictionary: res!)
+            UserDefaults.standard.set(res, forKey: "profile")
+            self.setUpData()
+            self.successCall()
+        })
+        
     }
     
-    func setUpData(){
+    // MARK: - Button Actions
+    
+    @IBAction func reloadProfile(_ sender: Any) {
+        getProfileData()
+    }
+    
+    @IBAction func logOut(_ sender: Any) {
+        AlertWith2ButtonsAndActionFirstButton(title: "😞", message: "you want to logOut ?", VC: self, B1Action: {
+            guard NetworkManager.sharedInstance.isConnected() else {
+                self.Alert(title: "Error!", message: "Please Connect to the Internet..", VC: self)
+                return
+            }
+            AccountManager.logOut(completionHandler: { (done, error) in
+                if error != nil {
+                    self.Alert(title: "Error!", message: error!, VC: self)
+                    return
+                }
+                self.logOut()
+            })
+        }, B1Title: "YES", B2Title: "NO")
+    }
+    
+    
+}
+
+// MARK: - Private Functions
+
+extension AccountViewController {
+    
+    private func logOut(){
+        UserDefaults.standard.removeObject(forKey: "sessionID")
+        UserDefaults.standard.removeObject(forKey: "profile")
+        AppDelegate.shared.rootViewController.switchToLogout()
+    }
+    
+    private func setUpData(){
         self.avatarImage.kf.indicatorType = .activity
         self.avatarImage.kf.setImage(with: URL(string: self.profile.gravatar), placeholder: #imageLiteral(resourceName: "placeholder"))
         UIView.animate(views: [imageView],
@@ -82,47 +122,16 @@ class AccountViewController: BaseViewController {
                        duration: 0.5)
     }
     
-    func faildCall(){
+    private func faildCall(){
         self.loading.stopAnimating()
         self.contentView.isHidden = true
         self.reloadButton.isHidden = false
     }
     
-    func successCall(){
+    private func successCall(){
         self.loading.stopAnimating()
         self.contentView.isHidden = false
         self.reloadButton.isHidden = true
     }
     
-    // MARK: - Button Actions
-    @IBAction func reloadProfile(_ sender: Any) {
-        getProfileData()
-    }
-    
-    @IBAction func logOut(_ sender: Any) {
-        AlertWith2ButtonsAndActionFirstButton(title: "😞", message: "you want to logOut ?", VC: self, B1Action: {
-            if NetworkManager.sharedInstance.isConnected() {
-                let parameters = [
-                    "session_id": UserDefaults.standard.string(forKey: "sessionID")
-                ]
-                var headers = [requestHeaders]()
-                headers.append(requestHeaders(key:"Content-Type",value:"application/json"))
-                
-                NetworkManager.sharedInstance.serverRequests(url: "https://api.themoviedb.org/3/authentication/session?api_key=\(Constants.api.api_key.rawValue)", method: .delete, parameters: parameters as [String : Any], headers: headers, success: { (res) in
-                    self.logOut()
-                }) { (error) in
-                    self.Alert(title: "Error!", message: error["status_message"] as? String ?? "Error", VC: self)
-                }
-            } else {
-                self.Alert(title: "Error!", message: "Please Connect to the Internet..", VC: self)
-            }
-        }, B1Title: "YES", B2Title: "NO")
-    }
-    
-    // MARK: - LogOut
-    func logOut(){
-        UserDefaults.standard.removeObject(forKey: "sessionID")
-        UserDefaults.standard.removeObject(forKey: "profile")
-        AppDelegate.shared.rootViewController.switchToLogout()
-    }
 }
